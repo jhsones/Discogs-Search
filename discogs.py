c0c6c3c4206bddf02ab1754396c6ce950e85f7a9
jhsones <jsones96@gmail.com>
2018-08-10 18:49:46 -0700

    initial commit

---
+++ discogs.py
@@ -0,0 +1,24 @@
+import urllib2
+import json
+import config
+
+def search(q):
+    q = q.replace(' ', '+')
+    url = "https://api.discogs.com/database/search?q=" + q + "&type=master&token=" + config.TOKEN
+    json_obj = urllib2.urlopen(url)
+    return json.load(json_obj)
+
+while (True): 
+    query = raw_input("Search album: ")
+    if (query == "quit"):
+        break
+    try:
+        data = search(query)["results"][0]
+        artist, album= data["title"].split(' - ')
+        year, label = data["year"], data["label"][0]
+        print "\nTitle: " + album + '\n', "Artist: " + artist + '\n',"Year: " + year + '\n', "Label: " + label + '\n'
+    except:
+        print "\nNo results found.\n "
+
+
+
