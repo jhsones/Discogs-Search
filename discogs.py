dabe4bda6f4a5b0cff523221c7936f977c6bcce4
jhsones <jsones96@gmail.com>
2018-08-13 15:32:39 -0700

    added price suggestion

--- discogs.py
+++ discogs.py
@@ -1,12 +1,19 @@
 import urllib2
 import json
 import config
+import requests
 
 def search(q):
     q = q.replace(' ', '+')
-    url = "https://api.discogs.com/database/search?q=" + q + "&type=master&token=" + config.api_token
+    url = "https://api.discogs.com/database/search?q=" + q + "&format_exact=Vinyl&type=release&token=" + config.api_token
     json_obj = urllib2.urlopen(url)
     return json.load(json_obj)
+
+def suggested_price(ID):
+    url = "https://api.discogs.com/marketplace/price_suggestions/" + ID
+    headers = {'Authorization': 'Discogs token=' + config.api_token}
+    r = requests.get(url, headers=headers)
+    return r.json()
 
 while (True): 
     query = raw_input("Search album: ")
@@ -16,7 +23,10 @@ while (True):
         data = search(query)["results"][0]
         artist, album= data["title"].split(' - ')
         year, label = data["year"], data["label"][0]
-        print "\nTitle: " + album + '\n', "Artist: " + artist + '\n',"Year: " + year + '\n', "Label: " + label + '\n'
+        ID = str(data["id"])
+        price = suggested_price(ID)
+        print "\nTitle: " + album + '\n', "Artist: " + artist + '\n',"Year: " + year + '\n', "Label: " + label + '\n', "ID: " + ID + '\n'
+        print json.dumps(price, indent=4, sort_keys=True)
     except:
         print "\nNo results found.\n "
 
